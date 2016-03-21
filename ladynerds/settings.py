6830abb9ff110c459917c3ed06b2b58837569205
Andrée Brazeau <abrazeau@bitcasa.com>
2016-03-20 22:48:48 -0700

    More settings.

--- ladynerds/settings.py
+++ ladynerds/settings.py
@@ -22,6 +22,15 @@ TEMPLATE_DIRS = (
     os.path.join(SETTINGS_PATH, 'templates'),
 )
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 # TEMPLATE_DIRS = (
@@ -115,17 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
-
-STATICFILES_DIRS = (
-    os.path.join(SETTINGS_PATH, 'static'),
-)
 
 MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
 MAILGUN_PASSWORD = os.environ.get('MAILGUN_PASSWORD')
